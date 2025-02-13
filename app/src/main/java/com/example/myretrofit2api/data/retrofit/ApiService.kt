f2c03e08429365e74e70551e077531c8e46a716a
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-13 14:42:54 +0700

    Mengubah ApiService.kt

--- app/src/main/java/com/example/myretrofit2api/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/myretrofit2api/data/retrofit/ApiService.kt
@@ -1,10 +1,15 @@
 package com.example.myretrofit2api.data.retrofit
 
 import com.example.myretrofit2api.data.response.ListDoaResponse
+import com.example.myretrofit2api.data.response.ListDoaResponseItem
 import retrofit2.Call
 import retrofit2.http.*
 
 interface ApiService {
+
     @GET("api/doa/v1/random")
-    fun getRandomDoa(): Call<ListDoaResponse>
+    fun getRandomDoa(): Call<List<ListDoaResponseItem>>
+
+    @GET("api")
+    fun getListDoa(): Call<List<ListDoaResponseItem>>
 }
