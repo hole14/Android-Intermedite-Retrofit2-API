eb76d2d8c788fb14da875eeda5eef22aa253b22f
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-13 10:21:44 +0700

    Menghapus DoaRandomResponse dan DoaRandomResponseItem

--- app/src/main/java/com/example/myretrofit2api/data/response/DoaRandomResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myretrofit2api.data.response
-
-import com.google.gson.annotations.SerializedName
-
-data class DoaRandomResponse(
-
-	@field:SerializedName("DoaRandomResponse")
-	val doaRandomResponse: List<DoaRandomResponseItem>
-)
