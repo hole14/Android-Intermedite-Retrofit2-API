002b253fd066c682cec4499087cffdb2d22376e8
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-13 20:53:59 +0700

    Mengubah DoaAdapter.kt

--- app/src/main/java/com/example/myretrofit2api/ui/DoaAdapter.kt
+++ app/src/main/java/com/example/myretrofit2api/ui/DoaAdapter.kt
@@ -1,14 +1,18 @@
 package com.example.myretrofit2api.ui
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myretrofit2api.data.response.ListDoaResponseItem
 import com.example.myretrofit2api.databinding.ItemDoaBinding
 
-class DoaAdapter(private val listDoa: ArrayList<String>): RecyclerView.Adapter<DoaAdapter.ViewHolder>() {
+class DoaAdapter(private val listDoa: ArrayList<ListDoaResponseItem>): RecyclerView.Adapter<DoaAdapter.ViewHolder>() {
     class ViewHolder(val binding: ItemDoaBinding): RecyclerView.ViewHolder(binding.root) {
         val tvItem = binding.tvItem
+        val tvAyat = binding.tvAyat
+        val tvLatin = binding.tvLatin
+        val tvArti = binding.tvArti
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -18,8 +22,25 @@ class DoaAdapter(private val listDoa: ArrayList<String>): RecyclerView.Adapter<D
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val doa = listDoa[position]
-        holder.tvItem.text = doa
+        holder.tvItem.text = doa.doa
+        holder.tvAyat.text = doa.ayat
+        holder.tvLatin.text = doa.latin
+        holder.tvArti.text = doa.artinya
+
+        holder.tvItem.setOnClickListener {
+            if (holder.tvAyat.visibility == View.VISIBLE){
+                holder.tvAyat.visibility = View.GONE
+                holder.tvLatin.visibility = View.GONE
+                holder.tvArti.visibility = View.GONE
+            }else{
+                holder.tvAyat.visibility = View.VISIBLE
+                holder.tvLatin.visibility = View.VISIBLE
+                holder.tvArti.visibility = View.VISIBLE
+            }
+
+        }
     }
 
     override fun getItemCount(): Int = listDoa.size
 }
+
