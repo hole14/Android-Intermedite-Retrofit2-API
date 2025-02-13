91c93569a9f34bf6de7c716bb11032f725c3698d
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-13 09:35:44 +0700

    Memindahkan ke packed ui

--- app/src/main/java/com/example/myretrofit2api/ui/MainActivity.kt
+++ app/src/main/java/com/example/myretrofit2api/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.myretrofit2api
+package com.example.myretrofit2api.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myretrofit2api.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
