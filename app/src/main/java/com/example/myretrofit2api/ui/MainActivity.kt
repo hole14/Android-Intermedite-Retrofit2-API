2182e98ab9b944edc4d568da256ea7b624262db7
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-13 10:33:26 +0700

    Mengedit MainActivity.kt

--- app/src/main/java/com/example/myretrofit2api/ui/MainActivity.kt
+++ app/src/main/java/com/example/myretrofit2api/ui/MainActivity.kt
@@ -1,21 +1,60 @@
 package com.example.myretrofit2api.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myretrofit2api.R
+import com.example.myretrofit2api.data.response.ListDoaResponse
+import com.example.myretrofit2api.data.retrofit.ApiConfig
+import com.example.myretrofit2api.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val TAG = "MainActivity"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        getRandomDoa()
+    }
+
+    private fun getRandomDoa() {
+        showLoading(true)
+
+        val client = ApiConfig.getApiService().getRandomDoa()
+        client.enqueue(object : Callback<ListDoaResponse> {
+
+            override fun onFailure(call: Call<ListDoaResponse>, t: Throwable) {
+
+            }
+
+            override fun onResponse(
+                call: Call<ListDoaResponse>,
+                response: Response<ListDoaResponse>
+            ) {
+
+            }
+
+        })
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading){
+            binding.progressBar.visibility = View.VISIBLE
+        }else{
+            binding.progressBar.visibility = View.GONE
         }
     }
+
 }
