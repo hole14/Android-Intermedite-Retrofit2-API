af23f0c788421770f898d9ce5122d9ceddcbc1b8
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-13 10:22:25 +0700

    Menambahkan ApiService.kt dan ApiConfig.kt

--- app/src/main/java/com/example/myretrofit2api/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/myretrofit2api/data/retrofit/ApiService.kt
@@ -1,4 +1,10 @@
 package com.example.myretrofit2api.data.retrofit
 
+import com.example.myretrofit2api.data.response.ListDoaResponse
+import retrofit2.Call
+import retrofit2.http.*
+
 interface ApiService {
+    @GET("api/doa/v1/random")
+    fun getRandomDoa(): Call<ListDoaResponse>
 }
